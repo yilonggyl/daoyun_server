cc1a3fc8107d5d282e870bb482954fd2da726745
ahtchxw <42773002+ahtchxw@users.noreply.github.com>
2019-05-06 14:40:25 +0800

    Update AuthroutesApiController.java

--- src/main/java/com/zw/admin/server/controller/apicontroller/AuthroutesApiController.java
+++ src/main/java/com/zw/admin/server/controller/apicontroller/AuthroutesApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+#123456
 @RestController
 @EnableAutoConfiguration
 public class AuthroutesApiController {
